a505794fbbe7c3dc26faa10de988167370d24bc0
delasalle-sio-dumas-b <dumas.benjamin.sio@gmail.com>
2018-10-16 15:51:00 +0200

    OUI 4

--- modele/DAO.test.php
+++ modele/DAO.test.php
@@ -497,6 +497,20 @@ else {
 
 
 
+// --------------------------------------------------------------------------------------
+// début de la zone attribuée au développeur 4 (DUMAS Benjamin) : lignes 500 à 599
+// --------------------------------------------------------------------------------------
+
+// test de la méthode supprimerUneTrace -----------------------------------------------------------
+// modifié par Jim le 15/8/2018
+echo "<h3>Test de supprimerUneTrace : </h3>";
+$ok = $dao->supprimerUneTrace(22);
+if ($ok) {
+    echo "<p>Trace bien supprimée !</p>";
+}
+else {
+    echo "<p>Echec lors de la suppression de la trace !</p>";
+}
 
 // ferme la connexion à MySQL :
 unset($dao);
