d1641d579b418648f73ec1800fa7096821caa9d9
BenjaDindou <b.dumas423@laposte.net>
2018-11-27 14:17:48 +0100

    RETIRER UNE AUTORISATION

--- services/RetirerUneAutorisation.php
+++ services/RetirerUneAutorisation.php
@@ -1,7 +1,23 @@
 <?php
 namespace services;
 
+use PDO;
+
 class RetirerUneAutorisation
 {
+    public function RetirerUneAutorisation($idAutorisant, $idAutorise)
+    {
+        // préparation de la requete de suppression
+        $txt_req = "DELETE from tracegps_autorisations where idAutorisant = :idAutorisant and idAutorise = :idAutorise ";
+        $req = $this->cnx->prepare($txt_req);
+        // liaison de la requête et de ses paramètres
+        $req->bindValue("idAutorisant", $idAutorisant, PDO::PARAM_STR);
+        $req->bindValue("idAutorise", $idAutorise, PDO::PARAM_STR);
+        // exécution de la requete
+        $ok = $req->execute();
+        
+        // fourniture de la réponse
+        return $ok;
+    }
 }
 
